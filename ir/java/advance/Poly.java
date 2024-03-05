d7189a69d2212685a1c4625ed4ac13efb0b9d8e7
mohammadpy8 <mohammadpy8@gmail.com>
2024-03-05 15:10:10 +0330

    abs

--- ir/java/advance/Poly.java
+++ ir/java/advance/Poly.java
@@ -25,9 +25,56 @@ class Children extends Poly {
         super.f();
     }
 }
-abstract class Animals{
+
+abstract class Animals {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public abstract void talk();
+
     public static void main(String[] args) {
 
+    }
+}
+
+class Animalss extends Animals {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void talk() {
+        System.out.println("talk is method abstract");
+    }
+}
+abstract class Shap{
+    public static void main(String[] args) {
+
+    }
+    public abstract double getArea();
+    public abstract double getPremeter();
+}
+class Rectangle extends Shap{
+    public static void main(String[] args) {
+        Rectangle rec = new Rectangle();
+        System.out.println(rec.getArea());
+        System.out.println(rec.getPremeter());
+    }
+
+    @Override
+    public double getPremeter() {
+        return 150;
+    }
+
+    @Override
+    public double getArea() {
+        return 120;
     }
 }
