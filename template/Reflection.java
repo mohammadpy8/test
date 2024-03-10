1d38ab92435ed6a9eed41948af967d133b1c8f35
mohammadpy8 <mohammadpy8@gmail.com>
2024-03-10 08:28:24 +0330

    ref

--- template/Reflection.java
+++ template/Reflection.java
@@ -1,7 +1,32 @@
 package template;
 
 public class Reflection {
-    public static void main(String[] args) {
+    static int s1 = f();
 
+    static {
+        System.err.println("static block");
+        s1 *= 2;
+    }
+
+    public static void g() {
+    }
+
+    private static int f() {
+        System.err.println("inline static init");
+        return 5;
+    }
+}
+class Statics {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Reflection e;
+        System.err.println("After Decelaration");
+        Reflection.g();
+        e = new Reflection();
+        e = new Reflection();
+        Reflection ref = new Reflection();
+        System.out.println("--------");
+        String ff = ref.getClass().getSimpleName();
+        System.out.println(ff);
+        Class.forName("a");
     }
 }
