e70a749640fd612f15974c873896432e3678ac14
mohammadpy8 <mohammadpy8@gmail.com>
2024-03-10 08:53:58 +0330

    FunctionalInterface

--- custom/Main.java
+++ custom/Main.java
@@ -7,11 +7,29 @@ public class Main {
 
     }
 }
-
-interface Person {
+@FunctionalInterface
+interface Person<E extends Number> {
     Date getBirthday();
+
     default Integer age() {
         long diff = new Date().getTime() - getBirthday().getTime();
         return (int) (diff / (1000L * 60 * 60 * 24 * 365));
+    }
+}
+
+class Student implements Person {
+    private Date birthDate;
+
+    @Override
+    public Date getBirthday() {
+        return birthDate;
+    }
+
+    @Override
+    public Integer age() {
+        long yearMilieSeconds = 100L * 60 * 60 * 24 * 360;
+        long currentYear = new Date().getTime() / yearMilieSeconds;
+        long birthYear = getBirthday().getTime() / yearMilieSeconds;
+        return (int) (currentYear - birthYear);
     }
 }
